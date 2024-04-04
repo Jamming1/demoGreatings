8065da79afbcba9d317ebc040f33e29807cb18b9
JAMMING <154436983+Jamming1@users.noreply.github.com>
2024-04-04 12:32:11 +0200

    GreatingsObj.java

--- src/main/java/com/example/demoGreatings/GreatingsObj.java
+++ src/main/java/com/example/demoGreatings/GreatingsObj.java
@@ -1,9 +1,8 @@
 package com.example.demoGreatings;
 public class GreatingsObj {
     public String nome;
-    public String saluto;
     public String regione;
-
+    public String saluto;
     public GreatingsObj(String nome, String regione, String saluto) {
         this.nome = nome;
         this.regione = regione;
